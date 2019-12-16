6fb077912276a63d4f9f07457dfe192eb67de417
Anton Andresen <anton_real_madrid@icloud.com>
2019-12-16 22:04:21 +0100

    separated aliases

---
+++ aliases.zsh
@@ -0,0 +1,8 @@
+# Git
+alias ga="git add ."
+alias gac="git add . && git commit -a -m "
+
+# Navigation
+alias help="vim ~/dotfiles/.help"
+alias vimrc="vim ~/dotfiles/.vimrc"
+alias aliases="vim ~/dotfiles/aliases.zsh"
